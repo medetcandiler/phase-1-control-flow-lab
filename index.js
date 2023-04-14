98b30d0a0052a85dc472864aee80790921a2a6f0
Medetcan Diler <medetdiler1996@gmail.com>
2023-04-14 18:10:04 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride) {
   // Write your code here!
+  let result;
+  if (ride <= 400) {
+    return result = 'This one is on me!'
+  } else if (ride > 400 && ride < 2000) {
+    return result = 'That will be twenty bucks.'
+  } else if (ride >= 2000 && ride <= 2500) {
+    return result = 'I will gladly take your thirty bucks.'
+  } else if (ride > 2500) {
+    return result = 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
+
+function ternaryCheckCity(city) {
   // Write your code here!
+  let result;
+  if (city === 'NYC') {
+    return result = 'Ok, sounds good.';
+  } else {
+    return result = 'No go.'
+  }
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip) {
   // Write your code here!
+  let result;
+  switch (true) {
+    case (tip === 'generous'):
+      result = 'Thank you so much.';
+      return result;
+      break;
+    case (tip === 'not as generous'):
+      result = 'Thank you.'
+      return result;
+      break;
+    case (tip === 'thanks for everything'):
+      result = 'Bye.';
+      return result;
+  }
 }
